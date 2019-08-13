e8deb9ac7127d32318e67c0ce09bb8f6d4b56f54
shirow ozawa <48234980+shirow-ozawa@users.noreply.github.com>
2019-08-13 19:09:02 +0900

    improve enter room sequence (#11)
    * I can somehow communicate (#6)
    
    * refactor:modify TURN username and password
    
    for security.
    
    * fix:fix cannot send message
    
    after all the remote channel seems necessary;)
    
    * feat:なんとかDataChannel open できるようになった
    
    ！注意！まだ投稿はできません！
    
    * refactor:change method name
    
    incorrect.
    
    * style:add and delete comment
    
    * fix:関数の実装を修正および新関数（テスト用）追加
    
    ルーム名が間違っていた
    
    * fix:DataChannel open ができなかった問題を修正
    
    ！応急処置なのであとであらためてエラー処理を実装すべし！
    
    * style:delete comment
    
    * style:sdd and delete comment
    
    * style:delete trailing space
    
    * refactor:メンバー名の取得元および表示先を変更
    
    * style:不要な処理をコメントアウト
    
    * feat:メンバーIDを入力するエリアを追加
    
    開発用
    
    * style:update comment
    
    * feat:add login modal
    
    * style:不要な変数をコメントアウト
    
    なぜコメントアウトかというと、ビデオチャットを将来使いたくなるかもしれないから。
    
    * style:fix indent
    
    * refactor:form タグをコメントアウト
    
    form は要らない気がする
    
    * refactor:DOM要素からmembersを取得する処理をコメントアウト
    
    変数名としてmembersを使いたいので。
    
    * feat:ログイン処理を追加
    
    * feat:関数追加
    
    テスト用
    
    * refactor:最大接続相手数を1に制限
    
    マルチユーザは不要。
    
    * feat:ログインユーザ名を表示する機能を追加
    
    * style:不要コードの削除およびコメントアウト
    
    ストリーム関連のコードを削除しないのはビデオチャットを使いたくなるかもしれないから。
    
    * style:関数の宣言箇所を移動
    
    あっちこっちとんで読みにくいから。
    
    * docs:関数のJSDocコメントを追加
    
    * docs:関数にJSDocコメントを追加
    
    * feat:Firabase Authentication を使用して認証機能を追加
    
    * refactor:「別ウインドウで開く」リンクを削除
    
    テスト用のコードのため。
    
    * style:WebSocket に関するコメントを削除
    
    もう不要となったため。
    
    * style:不要コメント削除および空行追加
    
    読みやすさのため。
    
    * style:不要なコメントを削除
    
    ハンドシグナリングはもう使わないので。
    
    * style:不要なコメントを削除
    
    * refactor:Firebase initialize を別ファイル化した
    
    ウザいから。
    
    * refactor:change var to let
    
    varだとたぶんイクナイ。
    
    * refactor:Authenticateのパラメータを増やしてみた
    
    まだコメントだがあとから追加できるようにしておいた。
    
    * fix:末尾にセミコロン追加
    
    * feat:関数追加
    
    下記の関数を追加
    - Firabase Realtime Database 上にアカウントが存在するかを確認する関数
    - Firabase Realtime Database 上にアカウントが存在しなかった場合にアカウントおよびルームを作成する関数
    
    * style:不要コメントを削除
    
    デバッグ用関数のため。
    
    * style:不要なコメントを削除
    
    * test:IDトークンを取得するためのコードをコメントで記入
    
    なんかuidをやりとりするなみたいなことが書いてあったから入れてみたけど、不要っぽい。
    
    * style:不要コメント削除
    
    * style:不要コメント削除
    
    * refactor:ログインシーケンスを修正
    
    * refactor:ファイル名変更
    
    Googleのサンプルにならって。
    
    * style:不要なコードおよびコメントを削除
    
    * feat:サインアップ（OR サインイン）->ルーム作成までの実装を修正
    
    * docs:解析の際のヒントとなるコメントを追加
    
    * feat:ルームリンクを追加

---
+++ scripts/init-firebase.js
@@ -0,0 +1,14 @@
+// ----- use firebase.io ----
+// Initialize Firebase
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyC1OBg2dia_Gjca39qnhGJOugcqXI2RtQ4",
+	authDomain: "ninkatsu-sig-fire.firebaseapp.com",
+	databaseURL: "https://ninkatsu-sig-fire.firebaseio.com",
+	projectId: "ninkatsu-sig-fire",
+	storageBucket: "",
+	messagingSenderId: "706754252656",
+	appId: "1:706754252656:web:ba1736c22779caa1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
